24d4f5a860ee6be7fdc8767096c14d1b8bca3ac5
SusanWendt <wendt.37@osu.edu>
2018-03-04 22:09:15 -0500

    some css added

--- src/main/java/org/wecancodeit/reviewsitefullstack/ReviewPopulator.java
+++ src/main/java/org/wecancodeit/reviewsitefullstack/ReviewPopulator.java
@@ -24,14 +24,14 @@ public class ReviewPopulator implements CommandLineRunner {
 		Category categoryYoga = new Category("Yoga", "description of Yoga category... ", "./images/dogYoga.jpg");
 		categoryYoga = catRepo.save(categoryYoga);
 
-		Category categoryBarre = new Category("Barre", "description of Barre category... ", "./images/dogYoga.jpg");
+		Category categoryBarre = new Category("Barre", "description of Barre category... ", "./images/barre.jpg");
 		categoryBarre = catRepo.save(categoryBarre);
 
 		Category categoryPilates = new Category("Pilates", "description of Pilates category... ",
-				"./images/dogYoga.jpg");
+				"./images/pilates.jpg");
 		categoryPilates = catRepo.save(categoryPilates);
 
-		Category categoryMix = new Category("Mixed Workout", "description of Mix category... ", "./images/dogYoga.jpg");
+		Category categoryMix = new Category("Mixed Workout", "description of Mix category... ", "./images/mix.jpg");
 		categoryMix = catRepo.save(categoryMix);
 
 		// tags
@@ -48,25 +48,6 @@ public class ReviewPopulator implements CommandLineRunner {
 		tag4 = tagRepo.save(tag4);
 
 		// reviews
-		Review reviewYoga1 = new Review("Yoga1", "imageURL", "websiteink", "instalink", "description", categoryYoga,
-				tag1, tag2);
-		reviewYoga1 = reviewRepo.save(reviewYoga1);
-
-		Review reviewYoga2 = new Review("Yoga2", "imageURL", "websiteink", "instalink", "description", categoryYoga,
-				tag1);
-		reviewYoga2 = reviewRepo.save(reviewYoga2);
-
-		Review reviewBarre1 = new Review("Barre1", "imageURL", "websiteink", "instalink", "description", categoryBarre,
-				tag3, tag4);
-		reviewBarre1 = reviewRepo.save(reviewBarre1);
-
-		Review reviewPilates1 = new Review("Pilates1", "imageURL", "websiteink", "instalink", "description",
-				categoryPilates, tag3);
-		reviewPilates1 = reviewRepo.save(reviewPilates1);
-
-		Review reviewMix1 = new Review("mix1", "imageURL", "websiteink", "instalink", "description", categoryMix, tag4,
-				tag2);
-		reviewMix1 = reviewRepo.save(reviewMix1);
 
 		Review yohi = new Review("Yoga on High", "./images/yohi.jpg", "http://yogaonhigh.com/",
 				"https://www.instagram.com/yogaonhigh/",
@@ -77,22 +58,22 @@ public class ReviewPopulator implements CommandLineRunner {
 		yohi = reviewRepo.save(yohi);
 
 		Review corePower = new Review("CorePower Yoga", "./images/corePower.jpg", "https://www.corepoweryoga.com/",
-				"https://www.instagram.com/corepoweryoga/?hl=en", "Description", categoryYoga, tag4);
+				"https://www.instagram.com/corepoweryoga/", "Description", categoryYoga, tag4);
 		corePower = reviewRepo.save(corePower);
 
 		Review barreThree = new Review("Barre3", "./images/barre3.png", "https://barre3.com/",
-				"https://www.instagram.com/",
+				"https://www.instagram.com/barre3/",
 				"Barre3 incorporates cardio and weights throughout the workout to give you a total body workout. ",
 				categoryBarre, tag2);
 		barreThree = reviewRepo.save(barreThree);
 
 		Review butcherShop = new Review("The ButcherShop Fitness", "./images/lagree.jpg",
-				"https://www.butchershopfitness.com/", "https://www.instagram.com/",
+				"https://www.butchershopfitness.com/", "https://www.instagram.com/lagreefitness/",
 				"Similar to barre, this workout targets muscles you've never worked before.", categoryPilates, tag3);
 		butcherShop = reviewRepo.save(butcherShop);
 
 		Review sos = new Review("System of Strength", "./images/sos.jpg", "https://systemofstrength.com/",
-				"https://www.instagram.com/", "Various workout classes that range from HIIT to Yoga.", categoryMix,
+				"https://www.instagram.com/systemofstrength/", "Various workout classes that range from HIIT to Yoga.", categoryMix,
 				tag4);
 		sos = reviewRepo.save(sos);
 
